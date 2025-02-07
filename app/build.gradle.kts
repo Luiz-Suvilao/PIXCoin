d07fb4a54784f1d7d8063098d37068cbf4818874
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-02-07 18:42:15 -0300

    feat: add lifecycle library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 }
