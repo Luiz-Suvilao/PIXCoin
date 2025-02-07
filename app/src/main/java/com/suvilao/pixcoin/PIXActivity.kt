b4f82aa3a9b6051a9a8b2638f3eca51536d53b35
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-02-07 17:54:29 -0300

    feat: create view of payment pix

---
+++ app/src/main/java/com/suvilao/pixcoin/PIXActivity.kt
@@ -0,0 +1,11 @@
+package com.suvilao.pixcoin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PIXActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pix)
+    }
+}
