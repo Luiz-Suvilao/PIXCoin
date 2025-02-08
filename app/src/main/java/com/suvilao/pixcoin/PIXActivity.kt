dd9d37dae126eb06d18d158752b75fa773e3ccbd
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-02-07 21:41:40 -0300

    feat: clear PIXActivity

--- app/src/main/java/com/suvilao/pixcoin/PIXActivity.kt
+++ app/src/main/java/com/suvilao/pixcoin/PIXActivity.kt
@@ -2,14 +2,8 @@ package com.suvilao.pixcoin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.suvilao.pixcoin.services.PixCoinService
-import com.suvilao.pixcoin.viewModels.PixViewModel
 
 class PIXActivity : AppCompatActivity() {
-    private val apiService = PixCoinService.api;
-
-    private val pixViewModel = PixViewModel(apiService)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pix)
