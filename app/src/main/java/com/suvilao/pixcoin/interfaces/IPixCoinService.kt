56253de6300478b1b6ada90d6d854ba87ad4e84b
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-04-09 15:24:34 -0300

    feat: add endpoint to create payment order

--- app/src/main/java/com/suvilao/pixcoin/interfaces/IPixCoinService.kt
+++ app/src/main/java/com/suvilao/pixcoin/interfaces/IPixCoinService.kt
@@ -1,10 +1,19 @@
 package com.suvilao.pixcoin.interfaces
 
+import com.suvilao.pixcoin.responses.CreatePaymentOrderResponse
 import com.suvilao.pixcoin.responses.GetLittleMachineResponse
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface IPixCoinService {
     @GET("buscar-maquininha/{code}")
     suspend fun getLittleMachine(@Path("code") code: String): GetLittleMachineResponse
+
+    @POST("create-payment-order/{clientId}")
+    suspend fun createPaymentOrder(
+        @Path("clientId") clientId: String,
+        @Query("valor") valor: String
+    ): CreatePaymentOrderResponse
 }
---
+++ app/src/main/java/com/suvilao/pixcoin/responses/CreatePaymentOrderResponse.kt
@@ -0,0 +1,14 @@
+package com.suvilao.pixcoin.responses
+
+import com.google.gson.annotations.SerializedName
+
+data class QrCodeItem(
+    @SerializedName("rel") val rel: String,
+    @SerializedName("href") val href: String,
+    @SerializedName("media") val media: String,
+    @SerializedName("type") val type: String
+)
+
+data class CreatePaymentOrderResponse(
+    @SerializedName("qr_codes") val qrCodes: List<QrCodeItem>
+)
